333c18faf3288bd5301425b5f8976ef3d026851a
reforzaes <reforza.es@gmail.com>
2026-01-10 17:36:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  plugins: [react()],
+  base: '/AKPI/', // Esto asegura que las rutas de los assets apunten a /AKPI/assets/...
+})
     return {
       server: {
         port: 3000,
